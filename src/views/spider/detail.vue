3a13c41a50a27f18806da686f953ac5d0c8af69f
BillDowney <billdowney@163.com>
2020-10-13 22:36:02 +0800

    增加爬虫root模板，修改格式化文件配置

--- src/views/spider/detail.vue
+++ src/views/spider/detail.vue
@@ -1,37 +1,30 @@
 <template>
   <a-layout class="spider-detail-container">
     <a-layout-header>
-      <template v-for="(item,index) in headerBtns">
-        <a-tooltip
-          :key="'header-icon-' + index"
-          :title="item.title"
-          placement="bottom"
-          v-if="item.type === 'ant'"
-        >
+      <template v-for="(item, index) in headerBtns">
+        <a-tooltip :key="'header-icon-' + index" :title="item.title" placement="bottom" v-if="item.type === 'ant'">
           <a-icon :type="item.icon"></a-icon>
         </a-tooltip>
-        <a-divider
-          :key="'header-icon-' + index"
-          type="vertical"
-          v-else-if="item.type === 'divider'"
-        />
+        <a-divider :key="'header-icon-' + index" type="vertical" v-else-if="item.type === 'divider'" />
       </template>
     </a-layout-header>
     <a-layout>
-      <a-layout-sider width="50px">
+      <a-layout-sider width="100px">
         <div class="spider-toolbar-container" ref="spiderToolbarContainer"></div>
       </a-layout-sider>
       <a-layout-content class="spider-content">
         <div class="spider-editor-container" ref="spiderEditorContainer"></div>
       </a-layout-content>
     </a-layout>
-    <a-layout-footer></a-layout-footer>
+    <a-layout-footer class="spider-properties-container">
+      <component :is="currentTemplate" :editor="editor" :cell="selectCell" />
+    </a-layout-footer>
   </a-layout>
 </template>
 
 <script>
 import HeaderBtnsJson from './json/header-btns.json'
-import { SpiderEditor } from '@/libs/spidereditor/spider-editor'
+import { SpiderEditor, JsonProperty } from '@/libs/spidereditor/spider-editor'
 import { loadShapes } from '@/libs/spidereditor/editor'
 
 export default {
@@ -41,7 +34,24 @@ export default {
         flowId: ''
       },
       headerBtns: HeaderBtnsJson,
-      editor: null
+      editor: null,
+      selectCell: {
+        id: '',
+        data: new JsonProperty()
+      }
+    }
+  },
+  computed: {
+    currentTemplate() {
+      var template = ''
+      if (this.selectCell.edge === 1) {
+        template = 'edge'
+      } else if (this.selectCell.data && this.selectCell.data.get('shape') === undefined) {
+        template = 'root'
+      } else {
+        template = this.selectCell.data.get('shape') || 'root'
+      }
+      return () => import(`@/views/spider/templates/${template}`)
     }
   },
   methods: {
@@ -49,32 +59,15 @@ export default {
     bindKeyEvent() {
       window.onkeydown = e => {
         let key = e.key
-        let exclude = [
-          'F1',
-          'F2',
-          'F3',
-          'F4',
-          'F5',
-          'F6',
-          'F7',
-          'F8',
-          'F9',
-          'F10',
-          'F11',
-          'F12'
-        ]
+        let exclude = ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12']
         if (exclude.indexOf(key) === -1) {
           e.preventDefault()
         }
         this.headerBtns.forEach(element => {
           if (element.exeFunKey && element.exeFunKey.length > 0) {
             if (
-              (element.exeFunKey.indexOf('ctrl') !== -1 &&
-                e.ctrlKey === true &&
-                element.exeFunKey.indexOf(key.toLowerCase()) !== -1) ||
-              (element.exeFunKey.indexOf('alt') !== -1 &&
-                e.altKey === true &&
-                element.exeFunKey.indexOf(key.toLowerCase()) !== -1) ||
+              (element.exeFunKey.indexOf('ctrl') !== -1 && e.ctrlKey === true && element.exeFunKey.indexOf(key.toLowerCase()) !== -1) ||
+              (element.exeFunKey.indexOf('alt') !== -1 && e.altKey === true && element.exeFunKey.indexOf(key.toLowerCase()) !== -1) ||
               element.exeFunKey.indexOf(key.toLowerCase()) !== -1
             ) {
               console.log(element.title)
@@ -82,6 +75,10 @@ export default {
           }
         })
       }
+    },
+    // 选择cell触发的方法
+    onSelectedCell(cell) {
+      this.selectCell = cell
     },
     // 渲染spider editor
     renderSpiderEditor() {
@@ -99,7 +96,9 @@ export default {
   },
   mounted() {
     this.queryParam.flowId = this.$route.params.flowId
+    // 渲染面板
     this.renderSpiderEditor()
+    // 绑定键盘事件
     this.bindKeyEvent()
   },
   destroyed() {
---
+++ src/views/spider/templates/root.vue
@@ -0,0 +1,72 @@
+<template>
+  <a-tabs default-active-key="1" @change="callback" size="small">
+    <a-tab-pane key="1" tab="全局配置">
+      <a-form :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
+        <a-row>
+          <a-col :span="formItemLayout.colSpan">
+            <a-form-item label="爬虫名称">
+              <a-input placeholder="请输入爬虫名称" :size="size" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="formItemLayout.colSpan">
+            <a-form-item label="提交策略">
+              <a-select :size="size">
+                <a-select-option value="random">随机</a-select-option>
+                <a-select-option value="linked">顺序</a-select-option>
+                <a-select-option value="child">子优先</a-select-option>
+                <a-select-option value="parent">父优先</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :span="formItemLayout.colSpan">
+            <a-form-item label="最大线程数">
+              <a-input-number placeholder="请输入最大线程数" :size="size" :min="1" style="width: 100%;"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-tab-pane>
+    <a-tab-pane key="2" tab="全局参数">
+      <a-form :label-col="formItemLayout.labelCol" :wrapper-col="formItemLayout.wrapperCol">
+        <a-row>
+          <a-col :span="8">
+            <a-form-item label="Field A">
+              <a-input placeholder="input placeholder" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-tab-pane>
+    <a-tab-pane key="3" tab="全局Cookie">
+      Content of Tab Pane 3
+    </a-tab-pane>
+    <a-tab-pane key="4" tab="全局Header">
+      Content of Tab Pane 3
+    </a-tab-pane>
+  </a-tabs>
+</template>
+
+<script>
+export default {
+  name: 'SRoot',
+  props: {
+    editor: Object,
+    cell: Object
+  },
+  data() {
+    return {
+      formItemLayout: {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+        colSpan: 8
+      },
+      size: 'small'
+    }
+  },
+  methods: {
+    callback(key) {
+      console.log(key)
+    }
+  }
+}
+</script>
